b13ce0715fa883b8023b7a917107229135ed8814
24ProgrammReloyd87 <pavelshinkarev87@gmail.com>
2020-09-24 15:36:27 +0300

    homew4whole2

--- homework4/homework4/Generics/DataContainer.java
+++ homework4/homework4/Generics/DataContainer.java
@@ -2,11 +2,11 @@ package homework4.homework4.Generics;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Comparator;
 
 class DataContainer <T> {
     private T[] data;
     private int index = 0;
-
 
     DataContainer(Class<T> Integer) {
         this.data = (T[]) Array.newInstance(Integer, 5);
@@ -22,11 +22,11 @@ class DataContainer <T> {
     }
 
     T get(int index) {
-        return (T) data[index];
+        return (T)data[index];
     }
 
     T[] getItems() {
-        return this.data;
+        return (T[])this.data;
     }
 
     boolean delete(int index) {
@@ -47,13 +47,61 @@ class DataContainer <T> {
         return true;
     }
 
-    boolean delete(T item){
+    boolean deleteItem(T item) {
 
-        return 
+        for (int i = 0; i < this.data.length ; i++) {
+            if (this.data[i] == (T) item) {
+                boolean temp = delete(i);
+                return true;
+            }
+        }
+        System.out.println("No item");
+        return false;
+    }
+
+    void sort(Comparator<T> comparator){
+        int a = 0;                                    // считаем сколкьо null в массиве
+        for (int i = 0; i < this.data.length ; i++) {
+            if (this.data[i] == null) {
+                a += 1;
+            }
+        }
+        // создаем новый массив без null
+                T[] temp = Arrays.copyOf(data, this.data.length - a);
+                int k = 0;
+                for (int j = 0; j < this.data.length; j++) {
+                    temp[k] = this.data[j];
+                        k++;
+                this.data = temp;
+                }
+        T tmp; // сортиреум
+        for (int i = 0; i < this.data.length-1 ; i++) {
+            for (int j = 0; j < this.data.length-1; j++) {
+                if(comparator.compare(this.data[j], this.data[j + 1]) > 0){
+                    tmp = this.data[j];
+                    this.data[j] = this.data[j+1];
+                    this.data[j+1] = tmp;
+                }
+            }
+        }
     }
 
         @Override
         public String toString () {
-            return "data = " + Arrays.toString(data);
+            int a = 0;                                    // считаем сколкьо null в массиве
+            for (int i = 0; i < this.data.length; i++) {
+                if (this.data[i] == null) {
+                    a += 1;
+                }
+            }
+            // создаем новый массив без null
+            T[] temp = Arrays.copyOf(data, this.data.length - a);
+            int k = 0;
+            for (int j = 0; j < this.data.length; j++) {
+                temp[k] = this.data[j];
+                k++;
+                this.data = temp;
+            }
+            return "data = " + Arrays.toString(temp);
         }
     }
--- homework4/homework4/Generics/DataContainerMain.java
+++ homework4/homework4/Generics/DataContainerMain.java
@@ -1,16 +1,22 @@
 package homework4.homework4.Generics;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class DataContainerMain {
     public static void main(String[] args) {
     DataContainer<Integer> one = new DataContainer<>(Integer.class);
-        int a =one.add(48);
-        int b =one.add(555);
+    DataComporator comparator = new DataComporator();
+        int a =one.add(28);
+        int b =one.add(72);
         int c =one.add(61);
-        int d =one.add(68);
-        System.out.println(one.toString());
-        one.delete(3);
-        System.out.println(one.toString());
+        int d =one.add(21);
 
+
+
+        System.out.println(one.toString());
+        one.sort(comparator);
+        System.out.println(one.toString());
 
 
     }
